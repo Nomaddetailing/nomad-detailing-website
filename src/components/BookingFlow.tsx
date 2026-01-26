9be5bc062f55e545305ec485a13ec8d04d29faee
Nomaddetailing <hello@nomaddetailing.com>
2026-01-26 16:01:09 +0800

    Update BookingFlow.tsx

--- src/components/BookingFlow.tsx
+++ src/components/BookingFlow.tsx
@@ -1021,7 +1021,7 @@ const submit = async () => {
                       sessionStorage.setItem("nav:returnTo", "booking");
                       sessionStorage.setItem("nav:returnAnchorId", "booking-consent");
                     
-                      onNavigate("privacy-policy");
+                      onNavigate("PrivacyPolicyPage");
                     }}
                     className="underline hover:text-foreground"
                     >
